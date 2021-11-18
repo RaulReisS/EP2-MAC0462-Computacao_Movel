137c5a756217c2368d0c92e047bacc93993810de
Raul Reis <raul.soares@usp.br>
2021-11-18 20:28:32 -0300

    Adicionado um comentário na primeira tela

--- Recipe/app/src/main/java/br/com/raulreis/recipe/SplashActivity.kt
+++ Recipe/app/src/main/java/br/com/raulreis/recipe/SplashActivity.kt
@@ -11,6 +11,7 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        // Manipualação da SplashScreen
         Handler(Looper.getMainLooper()).postDelayed({
         startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
