f550dfb2f6a9d3820786ee9d6f4882b484d0b1ad
Raul Reis <raul.soares@usp.br>
2021-11-19 18:21:11 -0300

    Adicionado a funcionalidade de obter a localização atual do dispositivo

--- Recipe/app/src/main/java/br/com/raulreis/recipe/LoginActivity.kt
+++ Recipe/app/src/main/java/br/com/raulreis/recipe/LoginActivity.kt
@@ -1,11 +1,22 @@
 package br.com.raulreis.recipe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.raulreis.recipe.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val btnEntrar = binding.btnLoginEntrar
+    }
+
+    fun login(view: android.view.View) {
+        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+        finish()
     }
 }
--- Recipe/app/src/main/java/br/com/raulreis/recipe/MainActivity.kt
+++ Recipe/app/src/main/java/br/com/raulreis/recipe/MainActivity.kt
@@ -1,11 +1,121 @@
 package br.com.raulreis.recipe
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import br.com.raulreis.recipe.databinding.ActivityMainBinding
+import com.google.android.gms.location.*
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
+
+    private var PERMISSION_ID = 1000
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val txvTeste = binding.txvTeste
+        txvTeste.text = "Teste"
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        getLastLocation()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getLastLocation()
+    }
+
+    private fun getLastLocation() {
+        if(checkPermission()) {
+            if (isLocationEnabled()) {
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+                    var location = task.result
+                    if (location == null) {
+                        getNewLocation()
+                    }
+                    else {
+                        binding.txvTeste.text = "Lat:${location.latitude}\nLong:${location.longitude}"
+                    }
+                }
+            }
+            else {
+                Toast.makeText(this,"Please Enable your Location Service", Toast.LENGTH_SHORT).show()
+            }
+        }
+        else {
+            requestPermission()
+        }
+    }
+
+    private fun getNewLocation() {
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 2
+            if (checkPermission()) {
+            fusedLocationProviderClient!!.requestLocationUpdates(
+                locationRequest, locationCallback, Looper.getMainLooper()
+        )
+        }
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            var lasLocation = p0.lastLocation
+            binding.txvTeste.text = "Lat:${lasLocation.latitude}\nLong:${lasLocation.longitude}"
+        }
+    }
+
+    // Checando a permissão
+    private fun checkPermission() : Boolean {
+        if (
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        )
+        {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION),
+            PERMISSION_ID)
+    }
+
+    private fun isLocationEnabled() : Boolean {
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Debug", "You have the permission")
+            }
+        }
     }
 }
