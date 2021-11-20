73b4202719c084138c833c9277c778ac4e77092d
Raul Reis <raul.soares@usp.br>
2021-11-20 14:52:45 -0300

    Melhorando o código para obter a localização do dispositivo

--- Recipe/app/src/main/java/br/com/raulreis/recipe/MainActivity.kt
+++ Recipe/app/src/main/java/br/com/raulreis/recipe/MainActivity.kt
@@ -1,121 +1,133 @@
 package br.com.raulreis.recipe
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
+
 import android.content.pm.PackageManager
-import android.location.LocationManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Looper
 import android.util.Log
-import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import br.com.raulreis.recipe.databinding.ActivityMainBinding
-import com.google.android.gms.location.*
-import java.util.zip.Inflater
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GooglePlayServicesUtil
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 
-class MainActivity : AppCompatActivity() {
 
-    private var PERMISSION_ID = 1000
-    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    lateinit var locationRequest: LocationRequest
+class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    // Variáveis globais
+    val PERMISSION_REQUEST_CODE = 1001
+    val PLAY_SERVICE_RESOLUTION_REQUEST = 1000
+
+    var mGoogleApiClient : GoogleApiClient? = null
+    var mLocationRequest: LocationRequest? = null
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        val txvTeste = binding.txvTeste
-        txvTeste.text = "Teste"
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        getLastLocation()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        getLastLocation()
-    }
-
-    private fun getLastLocation() {
-        if(checkPermission()) {
-            if (isLocationEnabled()) {
-                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
-                    var location = task.result
-                    if (location == null) {
-                        getNewLocation()
-                    }
-                    else {
-                        binding.txvTeste.text = "Lat:${location.latitude}\nLong:${location.longitude}"
-                    }
-                }
-            }
-            else {
-                Toast.makeText(this,"Please Enable your Location Service", Toast.LENGTH_SHORT).show()
-            }
-        }
-        else {
-            requestPermission()
-        }
-    }
-
-    private fun getNewLocation() {
-        locationRequest = LocationRequest()
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest.interval = 0
-        locationRequest.fastestInterval = 0
-        locationRequest.numUpdates = 2
-            if (checkPermission()) {
-            fusedLocationProviderClient!!.requestLocationUpdates(
-                locationRequest, locationCallback, Looper.getMainLooper()
-        )
-        }
-    }
-
-    private val locationCallback = object : LocationCallback() {
-        override fun onLocationResult(p0: LocationResult) {
-            var lasLocation = p0.lastLocation
-            binding.txvTeste.text = "Lat:${lasLocation.latitude}\nLong:${lasLocation.longitude}"
-        }
-    }
-
-    // Checando a permissão
-    private fun checkPermission() : Boolean {
-        if (
-            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-        )
-        {
-            return true
-        }
-        return false
+        requestPermission()
+        if (checkPlayService())
+            buildGoogleApiClient()
     }
 
     private fun requestPermission() {
-        ActivityCompat.requestPermissions(this,
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION),
-            PERMISSION_ID)
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            {
+            requestPermissions(arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_CODE)
+        }
     }
 
-    private fun isLocationEnabled() : Boolean {
-        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-    }
-
-    @SuppressLint("MissingSuperCall")
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        if (requestCode == PERMISSION_ID) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.d("Debug", "You have the permission")
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (checkPlayService()) {
+                        buildGoogleApiClient()
+                    }
+                }
             }
         }
+    }
+
+    private fun checkPlayService() : Boolean {
+        val resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this)
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICE_RESOLUTION_REQUEST).show()
+            }
+            else {
+                Toast.makeText(applicationContext, "This device is not supported", Toast.LENGTH_SHORT).show()
+            }
+            return false
+        }
+        return true
+    }
+
+    private fun buildGoogleApiClient() {
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API).build()
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        createLocationRequest()
+    }
+
+    private fun createLocationRequest() {
+        mLocationRequest = LocationRequest()
+        mLocationRequest!!.interval = 10000
+        mLocationRequest!!.fastestInterval = 5000
+        mLocationRequest!!.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_CODE)
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this)
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        mGoogleApiClient!!.connect()
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        Log.i("ERROR", "Connection failed: ${p0.errorCode}")
+    }
+
+    override fun onLocationChanged(p0: Location?) {
+        binding.txvTeste.text = "Lat: ${p0!!.latitude}\nLong: ${p0!!.longitude}"
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (mGoogleApiClient != null)
+            mGoogleApiClient!!.connect()
+    }
+
+    override fun onDestroy() {
+        mGoogleApiClient!!.disconnect()
+        super.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkPlayService()
     }
 }
